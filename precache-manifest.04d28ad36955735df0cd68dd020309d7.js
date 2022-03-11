4193204b4225f42e0358510e4f0e8c103d3d41cb
webdev0202 <webdev0202@yandex.com>
2022-03-11 09:49:39 +0100

    new commit

--- precache-manifest.04d28ad36955735df0cd68dd020309d7.js
+++ precache-manifest.04d28ad36955735df0cd68dd020309d7.js
@@ -1,23 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9516e901d81acb2d9516f7cf21f1cd72",
+    "revision": "b5e2c62254608adeb2654c672682eaf2",
     "url": "/index.html"
   },
   {
-    "revision": "efc4b830a8833b724a51",
-    "url": "/static/css/main.c0cec087.chunk.css"
+    "revision": "77ede34d8c1aa8438c16",
+    "url": "/static/css/main.ce134b70.chunk.css"
   },
   {
     "revision": "acabb9b9ea2eca5292a8",
     "url": "/static/js/2.e43b5cbb.chunk.js"
   },
   {
-    "revision": "efc4b830a8833b724a51",
+    "revision": "77ede34d8c1aa8438c16",
     "url": "/static/js/main.56c9da19.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
     "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "eca530d9b11d3bfea7ad040277b6f17b",
+    "url": "/static/media/avatar.eca530d9.jpg"
   },
   {
     "revision": "a7f774cec8cb39cb194da8634541481e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f3d5467da5f570d45562304a05020661.js"
+  "/precache-manifest.04d28ad36955735df0cd68dd020309d7.js"
 );
 
 self.addEventListener('message', (event) => {
